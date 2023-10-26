223b0d7713e9d9c2e9d7f00b70426c3528e1f4cf
Dkanee <moussagb43@gmail.com>
2023-10-26 15:17:57 +0200

    tests serveurs

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -4,27 +4,43 @@ import axios from "axios";
 
 const LoginScreen = () => {
     const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [motdepasse, setPassword] = useState('');
 
     const handleLogin = async () => {
         try {
-            const response = await axios.post('https://asalomon.v70208.campus-centre.fr/api/login', {
-                email,
-                password,
+            const response = await fetch('https://asalomon.v70208.campus-centre.fr/api/login', {
+                method: 'POST',
+                headers: {
+                    'accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: 'moussagb43@gmail.com',
+                    motdepasse: 'Kirua112.',
+                }),
             });
 
-            if (response.data && response.data.success) {
-                // La connexion a réussi,"
-                console.log('Bien joué');
+            if (response.ok) {
+                const data = await response.json();
+
+                if (data && data.success) {
+                    // La connexion a réussi
+                    console.log('Bien joué');
+                } else {
+                    // Gérez l'échec de la connexion, par exemple, affichez un message d'erreur.
+                    console.error('Échec de la connexion');
+                }
             } else {
-                // Gérez l'échec de la connexion, par exemple, affichez un message d'erreur.
-                console.error('Échec de la connexion');
+                // Gérez les erreurs HTTP (par exemple, 401 Unauthorized, 500 Internal Server Error, etc.).
+                console.error('Erreur HTTP lors de la connexion:', response.status);
             }
         } catch (error) {
-            // Gérez les erreurs réseau ou autres erreurs
+            // Gérez les autres erreurs
             console.error('Erreur lors de la connexion :', error);
         }
     };
+
+
 
     return (
         <View style={styles.container}>
@@ -38,7 +54,7 @@ const LoginScreen = () => {
             <Text style={styles.label}>Mot de passe:</Text>
             <TextInput
                 style={styles.input}
-                value={password}
+                value={motdepasse}
                 onChangeText={setPassword}
                 placeholder="Entrez votre mot de passe"
                 secureTextEntry
