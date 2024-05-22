07662a703f87bdba4f203234c84380d1ce0cd933
Dkanee <95855547+Dkanee@users.noreply.github.com>
2024-05-22 15:21:43 +0200

    Add files via upload

--- component/Navbar.js
+++ component/Navbar.js
@@ -1,85 +1,82 @@
-import React, { Component, useContext } from "react";
+import React, {Component, useContext} from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import { styles } from "../styles/GlobalStyles";
 import { AntDesign, Entypo } from "@expo/vector-icons";
-import { AuthContext } from "../middleware/AuthContext";
-import { useHistoryNavigation } from "../middleware/NavigationHistoryContext"; // Importez le hook personnalisé
+import {AuthContext} from "../middleware/AuthContext";
+import { useHistoryNavigation } from '../middleware/NavigationHistoryContext'; // Importez le hook personnalisé
 
 export default function Navbar({ title, navigation, route }) {
-  const { isLoggedIn, logout } = useContext(AuthContext);
-  const { getPreviousRoute } = useHistoryNavigation(); // Utilisez le hook pour accéder aux fonctions du contexte
+    const { isLoggedIn, logout } = useContext(AuthContext);
+    const { getPreviousRoute } = useHistoryNavigation(); // Utilisez le hook pour accéder aux fonctions du contexte
 
-  const handleGoBack = () => {
-    const previousRoute = getPreviousRoute();
-    if (previousRoute) {
-      navigation.navigate(previousRoute);
-    } else {
-      navigation.goBack();
-    }
-  };
+    const handleGoBack = () => {
+        const previousRoute = getPreviousRoute();
+        if (previousRoute) {
+            navigation.navigate(previousRoute);
+        } else {
+            navigation.goBack();
+        }
+    };
 
-  return (
-    <View style={styles.containerNav}>
-      {/* Afficher l'icône utilisateur uniquement si l'utilisateur n'est pas connecté et si nous sommes sur l'écran "Accueil" */}
-      {!isLoggedIn && route.name === "Accueil" ? (
-        <TouchableOpacity onPress={() => navigation.navigate("Se connecter")}>
-          <Entypo name="user" size={24} color="white" />
-        </TouchableOpacity>
-      ) : (
-        // Si l'utilisateur est connecté ou si nous ne sommes pas sur l'écran "Accueil", afficher un espace vide de la même taille pour maintenir le layout
-        <View style={{ width: 0 }} />
-      )}
+    return (
 
-      {/* Condition pour afficher l'icône de retour, sauf sur l'écran "Accueil" */}
-      {route.name !== "Accueil" &&
-      route.name !== "Profil" &&
-      route.name !== "Détails de la commande" &&
-      route.name !== "Historique des commandes" &&
-      route.name !== "Déconnexion" ? (
-        <TouchableOpacity onPress={handleGoBack}>
-          <AntDesign name="arrowleft" size={24} color="white" />
-        </TouchableOpacity>
-      ) : (
-        // Si nous sommes sur l'écran "Accueil", afficher un espace vide pour maintenir le layout
-        <View style={{ width: 0 }} />
-      )}
+        <View style={styles.containerNav}>
+            {/* Afficher l'icône utilisateur uniquement si l'utilisateur n'est pas connecté et si nous sommes sur l'écran "Accueil" */}
+            {!isLoggedIn && route.name === "Accueil" ? (
+                <TouchableOpacity onPress={() => navigation.navigate("Se connecter")}>
+                    <Entypo name="user" size={24} color="white" />
+                </TouchableOpacity>
+            ) : (
+                // Si l'utilisateur est connecté ou si nous ne sommes pas sur l'écran "Accueil", afficher un espace vide de la même taille pour maintenir le layout
+                <View style={{ width: 0 }} />
+            )}
 
-      {isLoggedIn && route.name === "Profil" ? (
-        <TouchableOpacity onPress={() => navigation.navigate("Accueil")}>
-          <Entypo name="home" size={24} color="white" />
-        </TouchableOpacity>
-      ) : (
-        // Si l'utilisateur est connecté ou si nous ne sommes pas sur l'écran "Accueil", afficher un espace vide de la même taille pour maintenir le layout
-        <View style={{ width: 0 }} />
-      )}
+            {/* Condition pour afficher l'icône de retour, sauf sur l'écran "Accueil" */}
+            {route.name !== "Accueil" && route.name !== "Profil" && route.name !== "Détails de la commande" && route.name !== "Historique des commandes" ? (
+                <TouchableOpacity onPress={handleGoBack}>
+                    <AntDesign name="arrowleft" size={24} color="white" />
+                </TouchableOpacity>
+            ) : (
+                // Si nous sommes sur l'écran "Accueil", afficher un espace vide pour maintenir le layout
+                <View style={{ width: 0 }} />
+            )}
 
-      {isLoggedIn && route.name === "Historique des commandes" ? (
-        <TouchableOpacity onPress={() => navigation.navigate("Profil")}>
-          <AntDesign name="arrowleft" size={24} color="white" />
-        </TouchableOpacity>
-      ) : (
-        // Si l'utilisateur est connecté ou si nous ne sommes pas sur l'écran "Accueil", afficher un espace vide de la même taille pour maintenir le layout
-        <View style={{ width: 24 }} />
-      )}
+            {isLoggedIn && route.name === "Profil" ? (
+                <TouchableOpacity onPress={() => navigation.navigate("Accueil")}>
+                    <Entypo name="home" size={24} color="white" />
+                </TouchableOpacity>
+            ) : (
+                // Si l'utilisateur est connecté ou si nous ne sommes pas sur l'écran "Accueil", afficher un espace vide de la même taille pour maintenir le layout
+                <View style={{ width: 0 }} />
+            )}
 
-      {isLoggedIn && route.name === "Détails de la commande" ? (
-        <TouchableOpacity
-          onPress={() => navigation.navigate("Historique des commandes")}
-        >
-          <AntDesign name="arrowleft" size={24} color="white" />
-        </TouchableOpacity>
-      ) : (
-        // Si l'utilisateur est connecté ou si nous ne sommes pas sur l'écran "Accueil", afficher un espace vide de la même taille pour maintenir le layout
-        <View style={{ width: 24 }} />
-      )}
+            {isLoggedIn && route.name === "Historique des commandes" ? (
+                <TouchableOpacity onPress={() => navigation.navigate("Profil")}>
+                    <AntDesign name="arrowleft" size={24} color="white" />
+                </TouchableOpacity>
+            ) : (
+                // Si l'utilisateur est connecté ou si nous ne sommes pas sur l'écran "Accueil", afficher un espace vide de la même taille pour maintenir le layout
+                <View style={{ width: 24 }} />
+            )}
 
-      {/* Titre de la navigation */}
-      <Text style={styles.titleNav}>{title}</Text>
+            {isLoggedIn && route.name === "Détails de la commande" ? (
+                <TouchableOpacity onPress={() => navigation.navigate("Historique des commandes")}>
+                    <AntDesign name="arrowleft" size={24} color="white" />
+                </TouchableOpacity>
+            ) : (
+                // Si l'utilisateur est connecté ou si nous ne sommes pas sur l'écran "Accueil", afficher un espace vide de la même taille pour maintenir le layout
+                <View style={{ width: 24 }} />
+            )}
 
-      {/* Bouton pour ouvrir le menu drawer */}
-      <TouchableOpacity onPress={() => navigation.openDrawer()}>
-        <Entypo name="menu" size={24} color="white" />
-      </TouchableOpacity>
-    </View>
-  );
+
+            {/* Titre de la navigation */}
+            <Text style={styles.titleNav}>{title}</Text>
+
+            {/* Bouton pour ouvrir le menu drawer */}
+            <TouchableOpacity onPress={() => navigation.openDrawer()}>
+                <Entypo name="menu" size={24} color="white" />
+            </TouchableOpacity>
+        </View>
+
+    );
 }
--- component/Navigation.js
+++ component/Navigation.js
@@ -7,7 +7,7 @@ import Navbar from "../component/Navbar";
 import { AuthContext } from "../middleware/AuthContext";
 import Createacc from "../screens/Createacc";
 import Produits from "../screens/Produits";
-import { Entypo } from "@expo/vector-icons";
+import { Entypo} from "@expo/vector-icons";
 import Profil from "../screens/Profil";
 import Panier from "../screens/Panier";
 import RechercheProduits from "../screens/RechercheProduits";
@@ -15,198 +15,197 @@ import HistoriqueC from "../screens/HistoriqueC";
 import CommandeDetailsScreen from "../screens/CommandeDetailsScreen";
 import ModifyProfil from "../screens/ModifyProfil";
 import LoginScreen from "../screens/LoginScreen";
-import { AuthProvider } from "../middleware/AuthContext";
 const Drawer = createDrawerNavigator();
 
 export default function navigation() {
-  const { isLoggedIn, logout } = useContext(AuthContext);
-  console.log(isLoggedIn);
+    const { isLoggedIn, logout } = useContext(AuthContext);
+    console.log(isLoggedIn);
 
-  return (
-    <AuthProvider>
-      <NavigationContainer>
-        <Drawer.Navigator
-          initialRouteName={"Accueil"}
-          drawerContent={(props) => <CustomDrawer {...props} />}
-          screenOptions={{
-            headerShown: true,
-            drawerActiveBackgroundColor: "#6C534E",
-            drawerActiveTintColor: "#fff",
-            drawerInactiveTintColor: "#333",
-            drawerLabelStyle: {
-              marginLeft: -25,
-              fontSize: 15,
-            },
-            drawerPosition: "right",
-          }}
-        >
-          <Drawer.Screen
-            name="Accueil"
-            component={Home}
-            options={() => ({
-              header: (props) => <Navbar {...props} title="Accueil"></Navbar>,
-              drawerIcon: (color) => (
-                <Entypo name="home" size={22} color={"#582900"} />
-              ),
-            })}
-          />
 
-          {isLoggedIn && (
-            <Drawer.Screen
-              name="Profil"
-              component={Profil}
-              options={() => ({
-                header: (props) => <Navbar {...props} title="Profil"></Navbar>,
-                drawerIcon: (color) => (
-                  <Entypo name="user" size={22} color={"#582900"} />
-                ),
-              })}
-            />
-          )}
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator
+                initialRouteName={"Accueil"}
+                drawerContent={(props) => <CustomDrawer {...props} />}
+                screenOptions={{
+                    headerShown: true,
+                    drawerActiveBackgroundColor: "#6C534E",
+                    drawerActiveTintColor: "#fff",
+                    drawerInactiveTintColor: "#333",
+                    drawerLabelStyle: {
+                        marginLeft: -25,
+                        fontSize: 15,
+                    },
+                    drawerPosition: 'right',
+                }}
+            >
+                <Drawer.Screen
+                    name="Accueil"
+                    component={Home}
+                    options={() => ({
+                        header: (props) => <Navbar {...props} title="Accueil"></Navbar>,
+                        drawerIcon: (color) => (
+                            <Entypo name="home" size={22} color={'#582900'} />
+                        ),
+                    })}
+                />
 
-          {!isLoggedIn ? (
-            <Drawer.Screen
-              name="Se connecter"
-              component={LoginScreen}
-              options={() => ({
-                header: (props) => (
-                  <Navbar {...props} title="Se connecter"></Navbar>
-                ),
-                drawerIcon: (color) => (
-                  <Entypo name="login" size={22} color={"#582900"} />
-                ),
-              })}
-            />
-          ) : (
-            <Drawer.Screen
-              name="Deconnexion"
-              component={LoginScreen}
-              options={{
-                header: (props) => (
-                  <Navbar {...props} title="Se connecter"></Navbar>
-                ),
-                drawerLabel: "Déconnexion",
-                drawerIcon: ({ color }) => (
-                  <Entypo name="log-out" size={22} color={"#582900"} />
-                ),
-                listeners: ({ navigation }) => ({
-                  press: (e) => {
-                    e.preventDefault(); // Empêcher la navigation vers le composant associé à ce Drawer.Screen
-                    logout(navigation); // Déclencher la fonction de déconnexion en passant l'objet navigation
-                    // drawerItemStyle: { display: "none" },
-                  },
-                }),
-              }}
-            />
-          )}
-          {!isLoggedIn && (
-            <Drawer.Screen
-              name="Créer compte"
-              component={Createacc}
-              options={() => ({
-                header: (props) => (
-                  <Navbar {...props} title="Création de compte"></Navbar>
-                ),
-                drawerIcon: (color) => (
-                  <Entypo name="user" size={22} color={"#582900"} />
-                ),
-              })}
-            />
-          )}
+                {!isLoggedIn ? (
+                    <Drawer.Screen
+                        name="Se connecter"
+                        component={LoginScreen}
+                        options={() => ({
+                            header: (props) => <Navbar {...props} title="Se connecter"></Navbar>,
+                            drawerIcon: (color) => (
+                                <Entypo name="login" size={22} color={'#582900'} />
+                            ),
+                        })}
+                    />
+                ) : (
+                    <Drawer.Screen
+                        name="Deconnexion"
+                        component={LoginScreen}
+                        options={{
+                            header: (props) => <Navbar {...props} title="Se connecter"></Navbar>,
+                            drawerLabel: 'Déconnexion',
+                            drawerIcon: ({ color }) => <Entypo name="log-out" size={22} color={'#582900'} />,
+                            // Gérer la déconnexion lors du clic
+                            listeners: ({ navigation }) => ({
+                                press: (e) => {
+                                    e.preventDefault(); // Empêcher la navigation vers le composant associé à ce Drawer.Screen
+                                    logout(navigation); // Déclencher la fonction de déconnexion en passant l'objet navigation
+                                },
+                            }),
+                        }}
+                    />
 
-          <Drawer.Screen
-            name="Produits"
-            component={Produits}
-            options={() => ({
-              header: (props) => <Navbar {...props} title="Produits"></Navbar>,
-              drawerIcon: (color) => (
-                <Entypo name="shopping-bag" size={22} color={"#582900"} />
-              ),
-            })}
-          />
+                )}
+                {!isLoggedIn &&  (
+                    <Drawer.Screen
+                        name="Créer compte"
+                        component={Createacc}
+                        options={() => ({
+                            header: (props) => <Navbar {...props} title="Création de compte"></Navbar>,
+                            drawerIcon: (color) => (
+                                <Entypo name="user" size={22} color={'#582900'} />
+                            ),
 
-          <Drawer.Screen
-            name="Panier"
-            component={Panier}
-            options={() => ({
-              header: (props) => <Navbar {...props} title="Panier"></Navbar>,
-              drawerIcon: (color) => (
-                <Entypo name="shopping-cart" size={22} color={"#582900"} />
-              ),
-            })}
-          />
+                        })}
 
-          <Drawer.Screen
-            name="RechercheProduits"
-            component={RechercheProduits}
-            options={() => ({
-              header: (props) => (
-                <Navbar {...props} title="Recherche Produits"></Navbar>
-              ),
-              drawerIcon: (color) => (
-                <Entypo name="user" size={22} color={"#582900"} />
-              ),
-              drawerItemStyle: { display: "none" },
-            })}
-          />
-          <Drawer.Screen
-            name="Historique des commandes"
-            component={HistoriqueC}
-            options={() => ({
-              header: (props) => (
-                <Navbar {...props} title="Historique des commandes"></Navbar>
-              ),
-              drawerIcon: (color) => (
-                <Entypo name="user" size={22} color={"#582900"} />
-              ),
-              drawerItemStyle: { display: "none" },
-            })}
-          />
+                    />
+                )}
 
-          {/*<Drawer.Screen*/}
-          {/*    name="Détails de la commande"*/}
-          {/*    component={CommandeDetailsScreen}*/}
-          {/*    options={() => ({*/}
-          {/*        header: (props) => <Navbar {...props} title="Détails de la commande"></Navbar>,*/}
-          {/*        drawerIcon: (color) => (*/}
-          {/*            <Entypo name="user" size={22} color={'#582900'} />*/}
-          {/*        ),*/}
-          {/*        drawerItemStyle: { display: 'none' },*/}
+                <Drawer.Screen
+                    name="Produits"
+                    component={Produits}
+                    options={() => ({
+                        header: (props) => <Navbar {...props} title="Produits"></Navbar>,
+                        drawerIcon: (color) => (
+                            <Entypo name="shopping-bag" size={22} color={'#582900'} />
+                        ),
+                    })}
+                />
 
-          {/*    })}*/}
 
-          {/*/>*/}
+                <Drawer.Screen
+                    name="Panier"
+                    component={Panier}
+                    options={() => ({
+                        header: (props) => <Navbar {...props} title="Panier"></Navbar>,
+                        drawerIcon: (color) => (
+                            <Entypo name="shopping-cart" size={22} color={'#582900'} />
+                        ),
+                    })}
+                />
+                {isLoggedIn &&  (
+                <Drawer.Screen
+                    name="Profil"
+                    component={Profil}
+                    options={() => ({
+                        header: (props) => <Navbar {...props} title="Profil"></Navbar>,
+                        drawerIcon: (color) => (
+                            <Entypo name="user" size={22} color={'#582900'} />
+                        ),
 
-          <Drawer.Screen
-            name="Modifier Profil"
-            component={ModifyProfil}
-            options={() => ({
-              header: (props) => (
-                <Navbar {...props} title="Modifier Profil"></Navbar>
-              ),
-              drawerIcon: (color) => (
-                <Entypo name="user" size={22} color={"#582900"} />
-              ),
-              drawerItemStyle: { display: "none" },
-            })}
-          />
-          {isLoggedIn && (
-            <Drawer.Screen
-              name="Détails de la commande"
-              component={CommandeDetailsScreen}
-              options={() => ({
-                header: (props) => (
-                  <Navbar {...props} title="Détails de la commande"></Navbar>
-                ),
-                drawerIcon: (color) => (
-                  <Entypo name="user" size={22} color={"#582900"} />
-                ),
-                drawerItemStyle: { display: "none" },
-              })}
-            />
-          )}
-        </Drawer.Navigator>
-      </NavigationContainer>
-    </AuthProvider>
-  );
+                    })}
+
+                />
+                    )}
+
+
+                    <Drawer.Screen
+                        name="RechercheProduits"
+                        component={RechercheProduits}
+                        options={() => ({
+                            header: (props) => <Navbar {...props} title="Recherche Produits"></Navbar>,
+                            drawerIcon: (color) => (
+                                <Entypo name="user" size={22} color={'#582900'} />
+                            ),
+                            drawerItemStyle: { display: 'none' },
+
+                        })}
+
+                    />
+                <Drawer.Screen
+                    name="Historique des commandes"
+                    component={HistoriqueC}
+                    options={() => ({
+                        header: (props) => <Navbar {...props} title="Historique des commandes"></Navbar>,
+                        drawerIcon: (color) => (
+                            <Entypo name="user" size={22} color={'#582900'} />
+                        ),
+                        drawerItemStyle: { display: 'none' },
+
+                    })}
+
+                />
+
+                {/*<Drawer.Screen*/}
+                {/*    name="Détails de la commande"*/}
+                {/*    component={CommandeDetailsScreen}*/}
+                {/*    options={() => ({*/}
+                {/*        header: (props) => <Navbar {...props} title="Détails de la commande"></Navbar>,*/}
+                {/*        drawerIcon: (color) => (*/}
+                {/*            <Entypo name="user" size={22} color={'#582900'} />*/}
+                {/*        ),*/}
+                {/*        drawerItemStyle: { display: 'none' },*/}
+
+                {/*    })}*/}
+
+                {/*/>*/}
+
+
+                <Drawer.Screen
+                    name="Modifier Profil"
+                    component={ModifyProfil}
+                    options={() => ({
+                        header: (props) => <Navbar {...props} title="Modifier Profil"></Navbar>,
+                        drawerIcon: (color) => (
+                            <Entypo name="user" size={22} color={'#582900'} />
+                        ),
+                        drawerItemStyle: { display: 'none' },
+
+                    })}
+
+                />
+                {isLoggedIn &&  (
+                    <Drawer.Screen
+                        name="Détails de la commande"
+                        component={CommandeDetailsScreen}
+                        options={() => ({
+                            header: (props) => <Navbar {...props} title="Détails de la commande"></Navbar>,
+                            drawerIcon: (color) => (
+                                <Entypo name="user" size={22} color={'#582900'} />
+                            ),
+                            drawerItemStyle: { display: 'none' },
+
+                        })}
+
+                    />
+                )}
+
+
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
 }
